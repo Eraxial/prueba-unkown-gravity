e00fbb1f2c8d1d88f56ddfa4b38c2c40e3ac2618
Eduardo <eddyrm@outlook.com>
2024-06-09 13:56:22 +0200

    añadido portada responsive y con modo noche y dia

--- client/src/components/Card/AppCard.jsx
+++ client/src/components/Card/AppCard.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 import {
+  Avatar,
+  Box,
   Button,
   ButtonGroup,
   Card,
@@ -14,20 +16,38 @@ import {
 export const AppCard = ({ book }) => {
   return (
     <Card maxW="300px">
-      <CardBody>
-        <Image
-          src={`/assets/images/${book.photo}`}
-          alt={`Cover of ${book.name}`}
-          borderRadius="lg"
-        />
-        <Stack mt="6" spacing="3">
+      <CardBody p={0}>
+        <Box position="relative" role="group" transition="filter 0.5s">
+          <Image
+            src={`/assets/images/${book.photo}`}
+            alt={`Cover of ${book.name}`}
+            borderRadius="5px 5px 0 0"
+            aspectRatio="16/9"
+            objectFit="cover"
+            transition="filter 0.3s ease" // Añade la transición
+            _groupHover={{ filter: "brightness(0.4)" }} // Hace que la imagen se vea más oscura al hacer hover
+          />
+          <Text
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            opacity={0}
+            visibility="hidden"
+            transition="opacity 0.3s ease, visibility 0.3s ease" // Añade la transición
+            _groupHover={{ opacity: 1, visibility: "visible" }} // Cambia la opacidad y visibilidad al hacer hover
+          >
+            {book.name}
+          </Text>
+        </Box>
+        <Stack spacing="3" p="3">
           <Heading size="sm">{book.name}</Heading>
           <Text color="blue.600" fontSize="2xl">
             100€
           </Text>
         </Stack>
       </CardBody>
-      <CardFooter>
+      <CardFooter p="3">
         <ButtonGroup spacing="2">
           <Button variant="solid" colorScheme="blue">
             Buy now
@@ -35,6 +55,7 @@ export const AppCard = ({ book }) => {
           <Button variant="ghost" colorScheme="blue">
             Add to cart
           </Button>
+          <Avatar src="/assets/images/me.png" />
         </ButtonGroup>
       </CardFooter>
     </Card>
